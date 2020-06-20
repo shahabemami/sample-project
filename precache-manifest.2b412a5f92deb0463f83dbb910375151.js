52e912473b5c80bb64a43a0d73fad763af04106e
shahab emami <shahbemami95@gmail.com>
2020-06-20 18:19:30 +0430

    Updates

---
+++ precache-manifest.2b412a5f92deb0463f83dbb910375151.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f9e2d848f1aaeb960a50ce0941d363",
+    "url": "/sample-project/index.html"
+  },
+  {
+    "revision": "f40b874860d2df491c15",
+    "url": "/sample-project/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "b053f087deeae2a90f3a",
+    "url": "/sample-project/static/css/main.eda6aebc.chunk.css"
+  },
+  {
+    "revision": "f40b874860d2df491c15",
+    "url": "/sample-project/static/js/2.f5e1d1ce.chunk.js"
+  },
+  {
+    "revision": "3b8464eda45e1c213d7f6886a962bc0b",
+    "url": "/sample-project/static/js/2.f5e1d1ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b053f087deeae2a90f3a",
+    "url": "/sample-project/static/js/main.4618e53f.chunk.js"
+  },
+  {
+    "revision": "b949da82985d28f815d6",
+    "url": "/sample-project/static/js/runtime-main.1769a80e.js"
+  }
+]);
